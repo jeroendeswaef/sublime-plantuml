f234067fb6a330c606f2ff71e3f90edaba736660
Jeroen De Swaef <j@recallq.com>
2014-06-15 09:59:56 +0200

    working

--- ConvertUml.py
+++ ConvertUml.py
@@ -7,8 +7,26 @@ from subprocess import Popen, PIPE, STDOUT
 class ConvertUmlCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		urls = []
-		self.full_text = self.view.substr(sublime.Region(0, self.view.size()))
+		placeholder = '//=============='
+		full_text = self.view.substr(sublime.Region(0, self.view.size()))
 
+		placeholder_position = full_text.find(placeholder)
+		if placeholder_position == -1:
+			print("a")
+			syntax = full_text
+			insert_position = self.view.size()
+		else:
+			print("b")
+			#text_before_fold = full_text.split(placeholder, 1)
+			text_before_fold = full_text[:placeholder_position]
+			print (text_before_fold)
+			print (placeholder_position)
+			self.view.erase(edit, sublime.Region(placeholder_position, self.view.size()))
+			syntax = text_before_fold
+
+		print ("syntax: %s" % syntax)
+		self.view.insert(edit, self.view.size(), placeholder + '\n')
 		p = subprocess.Popen(["/usr/bin/java", "-jar", "/opt/plantuml.jar", "-p", "-tutxt"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-		stdout = p.communicate(input=bytes('A -> Z', 'UTF-8'))[0]
+		stdout = p.communicate(input=bytes(syntax, 'UTF-8'))[0]
+
 		self.view.insert(edit, self.view.size(), stdout.decode('UTF-8'))
